d26e06a8d3a951b6956a4f7226f5923ed699ce85
wqmeepo <627822626@qq.com>
2019-01-28 15:49:13 +0800

    tiaozhan 17

--- shiyanlougithub/spiders/git_py.py
+++ shiyanlougithub/spiders/git_py.py
@@ -8,7 +8,17 @@ class GitPySpider(scrapy.Spider):
     start_urls = json.load(open('/home/shiyanlou/shiyanlougithub/shiyanlougithub/ipAddr.json')).values()
     def parse(self, response):
         for i in response.css('li.col-12'):
-            yield ShiyanlougithubItem({
-                'name': i.css('a::text').extract_first().strip(),
-                'update_time': i.css('relative-time::attr(datetime)').extract_first()
-                })
+            item = ShiyanlougithubItem()
+            item['name'] = i.css('a::text').extract_first().strip()
+            item['update_time'] = i.css('relative-time::attr(datetime)').extract_first()
+            course_url = response.urljoin(i.xpath('.//a/@href').extract_first())
+            request = scrapy.Request(course_url,callback = self.parse_inter)
+            request.meta['item'] = item
+            yield request
+
+    def parse_inter(self, response):
+        item = request['item']
+        item['commits'] = response.xpath('//span[@class="num text-emphasized"]/text()').extract()[0].strip()
+        item['branches'] = response.xpath('//span[@class="num text-emphasized"]/text()').extract()[0].strip()
+        item['releases'] = response.xpath('//span[@class="num text-emphasized"]/text()').extract()[2].strip()
+        yield item 
